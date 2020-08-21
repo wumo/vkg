6a7babe92b96b272d506673d138fdfacd455ca54
wumo <wumo@outlook.com>
2020-08-21 14:44:10 +0800

    refactor

--- src/vkg/render/pass/postprocess/tonemap_pass.cpp
+++
@@ -1,7 +0,0 @@
-#include "tonemap_pass.hpp"
-#include "postprocess/tonemap_comp.hpp"
-
-namespace vkg {
-ToneMapPass::ToneMapPass(): ScreenPass(shader::postprocess::tonemap_comp_span) {}
-void ToneMapPass::updatePushConstant() { pushConstant.exposure = 1.0; }
-}
--- src/vkg/render/pass/postprocess/tonemap_pass.hpp
+++ src/vkg/render/pass/postprocess/tonemap_pass.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "screen_pass.hpp"
+#include "postprocess/tonemap_comp.hpp"
 
 namespace vkg {
 struct ToneMapPushConstant {
@@ -7,9 +8,9 @@ struct ToneMapPushConstant {
 };
 class ToneMapPass: public ScreenPass<ToneMapPushConstant> {
 public:
-  ToneMapPass();
+  ToneMapPass(): ScreenPass(shader::postprocess::tonemap_comp_span) {}
 
 private:
-  void updatePushConstant() override;
+  void updatePushConstant() override { pushConstant.exposure = 1.0; }
 };
 }
