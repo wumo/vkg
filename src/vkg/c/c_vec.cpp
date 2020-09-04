ca3f0616a917b79f114133b6ee48ac41d8cb6571
WuMo <wumo@outlook.com>
2020-09-04 14:59:54 +0800

    aabb

--- src/vkg/c/c_vec.cpp
+++ src/vkg/c/c_vec.cpp
@@ -1,1 +1,20 @@
 #include "c_vec.h"
+#include "vkg/render/model/aabb.hpp"
+using namespace vkg;
+
+void AABBMergePoint(caabb *aabb, cvec3 *p, uint32_t offset) {
+  auto *aabb_ = reinterpret_cast<AABB *>(aabb);
+  aabb_->merge(*(glm::vec3 *)((float *)p + offset));
+}
+void AABBMergeAABB(caabb *aabb, caabb *other) {
+  auto *aabb_ = reinterpret_cast<AABB *>(aabb);
+  aabb_->merge(*(AABB *)other);
+}
+void AABBTransformMatrix(caabb *aabb, cmat4 *matrix, uint32_t offset) {
+  auto *aabb_ = reinterpret_cast<AABB *>(aabb);
+  *aabb_ = aabb_->transform(*(glm::mat4 *)((float *)matrix + offset));
+}
+void AABBTransform(caabb *aabb, ctransform *transform) {
+  auto *aabb_ = reinterpret_cast<AABB *>(aabb);
+  *aabb_ = aabb_->transform(*(Transform *)transform);
+}
