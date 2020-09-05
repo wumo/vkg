262575fe10e75fa8ab0798fa714a1d31e3e723de
wumo <wumo@outlook.com>
2020-09-05 14:18:57 +0800

    update doc

--- src/vkg/render/model/atmosphere.cpp
+++ src/vkg/render/model/atmosphere.cpp
@@ -3,20 +3,26 @@ namespace vkg {
 
 auto AtmosphereSetting::isEnabled() const -> bool { return enabled_; }
 auto AtmosphereSetting::enable(bool enabled) -> void { enabled_ = enabled; }
-auto AtmosphereSetting::sunDirection() const -> const glm::vec3 & { return sunDirection_; }
+auto AtmosphereSetting::sunDirection() const -> const glm::vec3 & {
+  return sunDirection_;
+}
 void AtmosphereSetting::setSunDirection(const glm::vec3 &sunDirection) {
-  sunDirection_ = sunDirection;
+  sunDirection_ = glm::normalize(sunDirection);
 }
 auto AtmosphereSetting::earthCenter() const -> const glm::vec3 & { return earthCenter_; }
 void AtmosphereSetting::setEarthCenter(const glm::vec3 &earthCenter) {
   earthCenter_ = earthCenter;
 }
 auto AtmosphereSetting::sunIntensity() const -> float { return sunIntensity_; }
-void AtmosphereSetting::setSunIntensity(float sunIntensity) { sunIntensity_ = sunIntensity; }
+void AtmosphereSetting::setSunIntensity(float sunIntensity) {
+  sunIntensity_ = sunIntensity;
+}
 auto AtmosphereSetting::exposure() const -> float { return exposure_; }
 void AtmosphereSetting::setExposure(float exposure) { exposure_ = exposure; }
 auto AtmosphereSetting::sunAngularRadius() const -> double { return kSunAngularRadius_; }
 auto AtmosphereSetting::sunSolidAngle() const -> double { return kSunSolidAngle_; }
-auto AtmosphereSetting::lengthUnitInMeters() const -> double { return kLengthUnitInMeters_; }
+auto AtmosphereSetting::lengthUnitInMeters() const -> double {
+  return kLengthUnitInMeters_;
+}
 auto AtmosphereSetting::bottomRadius() const -> double { return kBottomRadius_; }
 }
--- test/Sample.cpp
+++ test/Sample.cpp
@@ -1,20 +1,3 @@
-# vkg
-
-Graphics Engine on Vulkan written in C/C++ 20
-
-- Deferred Shading
-- Cascaded Shadow Map
-- Atmospheric Rendering
-- Ray tracing on NVIDIA RTX graphics card
-- glTF models.
-
-![sample](doc/sample.gif)
-
-## Usage
-
-Sample code:
-
-```c++
 #include "vkg/render/renderer.hpp"
 #include "vkg/util/colors.hpp"
 #include "vkg/render/util/panning_camera.hpp"
@@ -79,55 +62,3 @@ auto main() -> int {
 
   return 0;
 }
-```
-
-
-
-## Build
-
-Requirements:
-* `python3` >= 3.7
-* `git` >= 2.0.0
-* `gcc` >= 10.0; `Visual Studio 2019`; `clang` >= 10.0
-* `cmake` >= 3.12
-* `conan` >= 1.28
-* Graphics driver that supports `Vulkan 1.2.0`
-* `RayTracing Feature` requires RTX 20 series graphics card.
-
-
-**Ubuntu**:
-```
-$ sudo apt install -y git gcc-10 cmake make
-# install conan
-$ pip install conan
-```
-
-**Windows**:
-
-Instal `Visual Studio 2019` or using [scoop](https://scoop.sh/) to install dependencies:
-
-```
-# install scoop
-$ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-$ iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-# install dependencies
-$ scoop install python git gcc cmake
-# install conan
-$ pip install conan
-```
-
-
-Add aditional conan repo:
-```
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan remote add wumo https://api.bintray.com/conan/wumo/public
-
-```
-
-**Build with cmake**:
-
-```
-mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release
-```
