401ce194bb4a293133977e610072f566da5df05e
wumo <wumo@outlook.com>
2020-09-05 15:51:31 +0800

    escape windown conflict

--- src/vkg/c/c_window.cpp
+++ src/vkg/c/c_window.cpp
@@ -1,30 +1,29 @@
 #include "c_window.h"
 #include "vkg/base/window.hpp"
 
-using namespace vkg;
 uint32_t WindowGetWidth(CWindow *window) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   return win->width();
 }
 uint32_t WindowGetHeight(CWindow *window) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   return win->height();
 }
 uint32_t WindowGetTitleLength(CWindow *window) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   return uint32_t(win->windowTitle().size());
 }
 void WindowGetTitle(CWindow *window, char *titleBuf) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   auto title = win->windowTitle();
   memcpy(titleBuf, title.c_str(), title.size());
 }
 void WindowSetTitle(CWindow *window, char *titleBuf, uint32_t size) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   win->setWindowTitle(std::string{titleBuf, size});
 }
 Input *WindowGetInput(CWindow *window) {
-  auto *win = reinterpret_cast<Window *>(window);
+  auto *win = reinterpret_cast<vkg::Window *>(window);
   return &win->input();
 }
 int32_t InputMousePosX(Input *input) { return input->mousePosX; }
